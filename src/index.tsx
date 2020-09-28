08b6fb9e9c4dd1a831e83320f24919da89e9a092
Mariusz Roliński <mariusz@mondoreale.pl>
2020-09-28 13:17:27 +0200

    Define Provider and useSubscription

---
+++ src/index.tsx
@@ -0,0 +1,103 @@
+import React, { createContext, useContext, FunctionComponent, useMemo, useEffect, useState, useRef } from 'react'
+import StreamrClient from 'streamr-client'
+import eq from 'deep-equal'
+
+const ClientContext: React.Context<typeof StreamrClient> = createContext(null)
+
+type Props = {
+    children: string,
+    autoConnect?: boolean,
+    autoDisconnect?: boolean,
+}
+
+export const Provider: FunctionComponent<Props> = React.memo(function ClientProvider({
+    children,
+    autoConnect = true,
+    autoDisconnect = false,
+    ...props
+}) {
+    const [client, setClient] = useState(null)
+
+    useEffect(() => {
+        const client = new StreamrClient({
+            autoConnect,
+            autoDisconnect,
+            ...props
+        })
+
+        setClient(client)
+
+        return () => {
+            const disconnect = async () => {
+                try {
+                    await client.disconnect()
+                } catch (_) { /* */ }
+            }
+
+            disconnect()
+        }
+    })
+
+    return (
+        <ClientContext.Provider value={client}>
+            {children}
+        </ClientContext.Provider>
+    )
+}, ({ children: _dontKnow, ...prevProps }, { children: _dontCare, ...nextProps }) => (
+    eq(prevProps, nextProps)
+))
+
+const useClient = () => (
+    useContext(ClientContext)
+)
+
+export const useSubscription = (subscriptionParams: object, onMessage: (message: any) => void, onError?: (error: any) => void) => {
+    const client = useClient()
+
+    const onMessageRef = useRef(onMessage)
+
+    useEffect(() => {
+        onMessageRef.current = onMessage
+    }, [onMessage])
+
+    const onErrorRef = useRef(onError)
+
+    useEffect(() => {
+        onErrorRef.current = onError
+    }, [onError])
+
+    const [params, setParams] = useState(subscriptionParams)
+
+    useEffect(() => {
+        if (!eq(params, subscriptionParams)) {
+            // Change current params only if – according to `eq` – they changed.
+            setParams(subscriptionParams)
+        }
+    }, [subscriptionParams])
+
+    useEffect(() => {
+        if (!client) {
+            return () => {
+                // No client -> no unsubbing.
+            }
+        }
+
+        const sub = (() => {
+            try {
+                return client.subscribe(params, (message: object) => {
+                    onMessageRef.current(message)
+                })
+            } catch (e) {
+                (onErrorRef.current || (() => console.log(e)))(e)
+            }
+
+            return null
+        })()
+
+        return () => {
+            if (sub) {
+                client.unsubscribe(sub)
+            }
+        }
+    }, [client, params])
+}
