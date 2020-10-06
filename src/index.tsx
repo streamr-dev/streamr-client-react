3cd3e432d9ea3e8964897b1413168c9bbad49eaa
Mariusz Roliński <mariusz@mondoreale.pl>
2020-10-06 11:22:59 +0200

    Export raw client context, too

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export { default as ClientContext } from './contexts/Client'
 export { default as useClient } from './hooks/useClient'
 export { default as useSubscription } from './hooks/useSubscription'
 
