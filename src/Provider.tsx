77433dbe7f04534215d1bb6fa7d4022eb36cc05f
Mariusz Roliński <mariusz@mondoreale.pl>
2022-12-27 15:53:34 +0100

    Make `useClient` load StreamrClient asynchronously

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,22 +1,14 @@
-import React, { ReactElement } from 'react'
+import React, { type ReactNode, type ReactElement } from 'react'
 import ClientContext from './ClientContext'
 import type { StreamrClientConfig } from 'streamr-client'
 import useClient from './useClient'
 
-export type Props = {
-    children: React.ReactNode,
-} & StreamrClientConfig
+export interface Props extends StreamrClientConfig {
+    children: ReactNode
+}
 
-export default function ClientProvider({ children, ...props }: Props): ReactElement | null {
+export default function Provider({ children, ...props }: Props): ReactElement | null {
     const client = useClient(props)
 
-    if (!client) {
-        return null
-    }
-
-    return (
-        <ClientContext.Provider value={client!}>
-            {children}
-        </ClientContext.Provider>
-    )
+    return <ClientContext.Provider value={client}>{children}</ClientContext.Provider>
 }
