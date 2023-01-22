b125c9e081340caec658ed214ac48b0fc030f0ab
Mariusz Roliński <mariusz@mondoreale.pl>
2023-01-22 23:52:10 +0100

    Webpack → rollup; tweaks, deps update, etc.

--- src/resend.ts
+++ src/resend.ts
@@ -1,4 +1,5 @@
-import type { ResendOptions, StreamrClient, Message, StreamDefinition } from 'streamr-client'
+import type { ResendOptions, StreamDefinition, StreamrClient } from 'streamr-client'
+import type { StreamMessage } from 'streamr-client-protocol'
 import { FlowControls, Options } from './subscribe'
 
 export default function resend(
@@ -6,11 +7,22 @@ export default function resend(
     options: ResendOptions,
     streamrClient: StreamrClient,
     { ignoreUndecodedMessages = false, onMessageError, onError }: Options = {}
-): FlowControls<Message> {
-    const rs = new ReadableStream<Message>({
-        async start(controller: ReadableStreamDefaultController<Message>) {
+): FlowControls<StreamMessage> {
+    let cancelled = false
+
+    const rs = new ReadableStream<StreamMessage>({
+        async start(controller: ReadableStreamDefaultController<StreamMessage>) {
             try {
                 const queue = await streamrClient.resend(stream, options)
+
+                if (cancelled) {
+                    return
+                }
+
+                // @ts-expect-error `onMessage` is internal. #lifehack
+                queue.onMessage.listen((streamMessage: StreamMessage) => {
+                    controller.enqueue(streamMessage)
+                })
 
                 // @ts-expect-error `onError` is internal.
                 queue.onError.listen((e: any) => {
@@ -28,15 +40,14 @@ export default function resend(
                         controller.enqueue(msg)
                     }
                 })
-
-                for await (const msg of queue) {
-                    controller.enqueue(msg)
-                }
             } catch (e) {
                 onError?.(e)
             } finally {
                 controller.close()
             }
+        },
+        cancel() {
+            cancelled = true
         },
     })
 
