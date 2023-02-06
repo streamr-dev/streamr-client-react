97bf610f42f7988c2de40f41e9a9e1a44e07173a
Mariusz Roliński <mariusz@mondoreale.pl>
2023-02-06 23:57:56 +0100

    Fix the build; update deps
    Explicitly tell rollup which files are external. If your directory is named `streamr-client-react` and you're marking `/react/` and `/streamr-client/` as external… then nothing is gonna be bundled. ;)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import { readFileSync } from 'fs'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 import babel from '@rollup/plugin-babel'
@@ -7,51 +6,36 @@ import url from '@rollup/plugin-url'
 function bundle(config) {
     return {
         ...config,
-        external: [/react/, /streamr-client/, /process/],
+        external: ['react', 'react/jsx-runtime', 'streamr-client', 'process', 'react-fast-compare'],
     }
 }
 
-const config = []
-
-JSON.parse(readFileSync('./exports.json')).forEach(([dist, inputs]) => {
-    inputs.forEach((input) => {
-        const i = typeof input === 'string' ? input : input[0]
-
-        const o = typeof input === 'string' ? input.split(/\//).pop().replace(/\..+/, '') : input[1]
-
-        config.push(
-            bundle({
-                input: i,
-                plugins: [
-                    url(),
-                    esbuild(),
-                    babel({
-                        babelHelpers: 'bundled',
-                        extensions: ['.ts', '.tsx'],
-                    }),
-                ],
-                output: [
-                    {
-                        file: `${dist}/${o}.js`,
-                        format: 'es',
-                    },
-                ],
-            })
-        )
-
-        config.push(
-            bundle({
-                input: i,
-                plugins: [url(), dts()],
-                output: [
-                    {
-                        file: `${dist}/${o}.d.ts`,
-                        format: 'es',
-                    },
-                ],
-            })
-        )
-    })
-})
-
-export default config
+export default [
+    bundle({
+        input: './src/index.ts',
+        plugins: [
+            url(),
+            esbuild(),
+            babel({
+                babelHelpers: 'bundled',
+                extensions: ['.ts', '.tsx'],
+            }),
+        ],
+        output: [
+            {
+                file: `./index.js`,
+                format: 'es',
+            },
+        ],
+    }),
+    bundle({
+        input: './src/index.ts',
+        plugins: [url(), dts()],
+        output: [
+            {
+                file: `./index.d.ts`,
+                format: 'es',
+            },
+        ],
+    }),
+]
